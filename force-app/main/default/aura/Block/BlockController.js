aa3a926531efc18a3b644d4ef6a6230234c25c5d
Pankaj Kumar <pankaj_kumar7@persistent.com>
2022-11-28 22:04:15 +0530

    counter feature working

--- force-app/main/default/aura/Block/BlockController.js
+++ force-app/main/default/aura/Block/BlockController.js
@@ -1,8 +1,13 @@
 ({
     blockClickHandler : function(component, event, helper) {
        let open = component.get("v.open");
+       // fetch label value
+       const label = component.get("v.value");
        if(open === false){
             component.set("v.open", true);
+            let compEvent = cmp.getEvent("onclick");
+            compEvent.setParams({"value" : label});
+            compEvent.fire();
        } 
     }
 })
