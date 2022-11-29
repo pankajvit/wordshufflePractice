036ce9e24d5422a9c0a0e413650ede3196eecf8a
Pankaj Kumar <pankaj_kumar7@persistent.com>
2022-11-29 09:56:40 +0530

    practicing the code component event and it's working

--- force-app/main/default/aura/Block/BlockController.js
+++ force-app/main/default/aura/Block/BlockController.js
@@ -2,9 +2,13 @@
     blockClickHandler : function(component, event, helper) {
        let open = component.get("v.open");
        // fetch label value
-       const label = component.get("v.value");
+       const label = component.get("v.label");
        if(open === false){
             component.set("v.open", true);
+            // fire the event
+            const compEvent = component.getEvent("onclick");
+            compEvent.setParams({value : label});
+            compEvent.fire();
        } 
     }
 })
--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -17,5 +17,20 @@
         let winWord = helper.getWinWord(wordsList);
         component.set("v.winWord", winWord);
         console.log("winning word is :"+winWord);
+    },
+
+    handleBlockComponentEvent : function(component, event, helper){
+        let count = component.get("v.count") + 1;
+        const value = event.getParam("value");
+        if(value === component.get("v.winWord")){
+            // user win
+            component.set("v.result", "YOU WIN !!");
+            console.log("you win !!!");
+        } else if(count === 3){
+            // user lose
+            component.set("v.result", "YOU LOOSE !!!");
+            console.log("you loose !!");
+        }
+        component.set("v.count", count);
     }
 })
